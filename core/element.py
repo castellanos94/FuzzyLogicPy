4589df9e0ce89a8736058b1ce150af1a093b7a52
aca810@gmail.com <aca810@gmail.com>
2020-08-28 22:17:07 -0500

    Enum NodeType

--- core/element.py
+++ core/element.py
@@ -1,26 +1,60 @@
+import enum
 from abc import ABC
 
 from core.membership_function import MembershipFunction
+
+
+class NodeType(enum.Enum):
+    NOT = 0
+    AND = 1
+    OR = 2
+    IMP = 3
+    EQV = 4
+    STATE = 5
+
+    def __repr__(self):
+        if self is NodeType.AND:
+            return "AND"
+        elif self is NodeType.NOT:
+            return "NOT"
+        elif self is NodeType.IMP:
+            return "IMP"
+        elif self is NodeType.EQV:
+            return "EQV"
+        elif self is NodeType.OR:
+            return "OR"
+        elif self is NodeType.STATE:
+            return "STATE"
+        raise Exception("Invalid")
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class Node(ABC):
     def __init__(self, label: str, editable: bool = False):
         self.label = label
         self.editable = editable
+        self.type = None
 
     def __str__(self):
         return self.label
 
 
 class Operator(Node):
-    def __init__(self, label: str, editable: bool = False):
-        super(Operator, self).__init__(label, editable)
+    def __init__(self, type_: NodeType, editable: bool = False):
+        super(Operator, self).__init__(str(type_), editable)
         self.children = []
+        self.type = type
+
+    def add_child(self, node: Node):
+        pass
 
 
 class StateNode(Node):
     def __init__(self, label: str, cname: str, membership: MembershipFunction = None, editable: bool = False):
         super().__init__(label, editable)
+        self.type = NodeType.STATE
         self.cname = cname
         self.membership = membership
 
@@ -33,4 +67,4 @@ class StateNode(Node):
 
 if __name__ == '__main__':
     quality = StateNode('high quality', 'quality')
-    print(quality)
+    print(quality, quality.type)
