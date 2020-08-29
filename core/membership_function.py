bf0add277c9183f2d57612e8874f537d438f8220
aca810@gmail.com <aca810@gmail.com>
2020-08-28 21:12:00 -0500

    Abstract membership_function.py class

---
+++ core/membership_function.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class MembershipFunction(ABC):
+    @abstractmethod
+    def evaluate(self, value) -> float:
+        pass
+
+    @abstractmethod
+    def derive(self, param: str) -> float:
+        pass
+
+    @abstractmethod
+    def name(self) -> str:
+        pass
