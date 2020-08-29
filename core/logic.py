477be82ab7ef5edbcf8da5fce9443a953f580367
aca810@gmail.com <aca810@gmail.com>
2020-08-29 16:31:29 -0500

    GMBC added to logic.py

--- core/logic.py
+++ core/logic.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from functools import reduce
 
 
 class Logic(ABC):
@@ -38,6 +39,12 @@ class Logic(ABC):
     def or_(self, values) -> float:
         pass
 
+    def name(self):
+        return str(self.__class__.__name__)
+
+    def __str__(self):
+        return self.name()
+
 
 class ZadehLogic(Logic):
     """
@@ -71,3 +78,37 @@ class ZadehLogic(Logic):
 
     def exist(self, values) -> float:
         return max(values)
+
+
+class GMBC(Logic):
+    """
+    Logica Compensatoria Basada en la Media Geometrica.
+    """
+
+    def not_(self, value: float) -> float:
+        return 1 - value
+
+    def and_test(self, a: float, b: float) -> float:
+        return pow(a * b, 0.5)
+
+    def or_test(self, a: float, b: float) -> float:
+        return 1 - pow((1 - a) * (1 - b), 0.5)
+
+    def imp_(self, a: float, b: float) -> float:
+        neg = 1 - a
+        return 1 - pow((1 - neg) * (1 - b), 0.5)
+
+    def eqv_(self, a: float, b: float) -> float:
+        return pow(self.imp_(a, b) * self.imp_(b, a), 0.5)
+
+    def for_all(self, values) -> float:
+        pass
+
+    def exist(self, values) -> float:
+        pass
+
+    def and_(self, values) -> float:
+        return pow(reduce(lambda x, y: x * y, values), 1 / len(values))
+
+    def or_(self, values) -> float:
+        return pow(1 - reduce(lambda x, y: (1 - x) * (1 - y), values), 1 / len(values))
