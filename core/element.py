08b5a0c157e6b310ef80772b43b5f17ccfa18a80
aca810@gmail.com <aca810@gmail.com>
2020-08-28 22:43:40 -0500

    Added GeneratorNode

--- core/element.py
+++ core/element.py
@@ -1,5 +1,6 @@
 import enum
 from abc import ABC
+from typing import List
 
 from core.membership_function import MembershipFunction
 
@@ -11,6 +12,7 @@ class NodeType(enum.Enum):
     IMP = 3
     EQV = 4
     STATE = 5
+    GENERATOR = 6
 
     def __repr__(self):
         if self is NodeType.AND:
@@ -25,6 +27,8 @@ class NodeType(enum.Enum):
             return "OR"
         elif self is NodeType.STATE:
             return "STATE"
+        elif self is NodeType.GENERATOR:
+            return "GENERATOR"
         raise Exception("Invalid")
 
     def __str__(self):
@@ -67,6 +71,8 @@ class Operator(Node):
                 raise RuntimeError("arity must be two element.")
         elif self.type is NodeType.OR:
             self.children.append(node)
+        else:
+            raise RuntimeError("error: " + str(self.type))
 
 
 class StateNode(Node):
@@ -83,6 +89,15 @@ class StateNode(Node):
         return self.__str__()
 
 
-if __name__ == '__main__':
-    quality = StateNode('high quality', 'quality')
-    print(quality, quality.type)
+class GeneratorNode(Node):
+    def __init__(self, depth: int, labels: List[str], operators=List[NodeType]):
+        super(GeneratorNode, self).__init__(str(NodeType.GENERATOR), editable=True)
+        self.labels = labels
+        self.operators = operators
+        self.depth = depth
+
+    def add_state(self, state: StateNode):
+        self.labels.append(state.label)
+
+    def __str__(self):
+        return '{} {} {} {}'.format(self.label, self.labels, self.operators, self.depth)
