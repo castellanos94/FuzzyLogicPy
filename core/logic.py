2b68f89f625b004c43be9559b22517362d467359
aca810@gmail.com <aca810@gmail.com>
2020-08-28 20:35:15 -0500

    Logic class base

--- core/logic.py
+++ core/logic.py
@@ -3,5 +3,37 @@ from abc import ABC, abstractmethod
 
 class Logic(ABC):
     @abstractmethod
-    def not(self, value:float) -> float:
+    def not_(self, value: float) -> float:
+        pass
+
+    @abstractmethod
+    def and_(self, a: float, b: float) -> float:
+        pass
+
+    @abstractmethod
+    def or_(self, a: float, b: float) -> float:
+        pass
+
+    @abstractmethod
+    def imp_(self, a: float, b: float) -> float:
+        pass
+
+    @abstractmethod
+    def eqv_(self, a: float, b: float) -> float:
+        pass
+
+    @abstractmethod
+    def for_all(self, values: List[float]) -> float:
+        pass
+
+    @abstractmethod
+    def exist(self, values: List[float]) -> float:
+        pass
+
+    @abstractmethod
+    def and_(self, values: List[float]) -> float:
+        pass
+
+    @abstractmethod
+    def or_(self, values: List[float]) -> float:
         pass
